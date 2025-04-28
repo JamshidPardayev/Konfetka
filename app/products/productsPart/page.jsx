4cc9bda7c31bd752655c749cec95c070678310ed
JamshidPardayev <jamshidnt2024@gmail.com>
2025-04-28 20:43:09 +0500

    Products qushildi

--- app/products/productsPart/page.jsx
+++ app/products/productsPart/page.jsx
@@ -3,39 +3,94 @@ import React from 'react';
 const products = [
   {
     id: 1,
-    name: 'Chocolate Bar',
-    cost: '$5.99',
-    image: 'https://example.com/chocolate1.jpg',
+    name: 'Ferrero Rosher',
+    cost: '$20.99',
+    image: '/image/ferrero.jpg',
   },
   {
     id: 2,
-    name: 'Dark Chocolate',
-    cost: '$7.99',
-    image: 'https://example.com/chocolate2.jpg',
+    name: 'Dubai Chocolate',
+    cost: '$50.99',
+    image: '/image/dubai.jpg',
   },
   {
     id: 3,
-    name: 'Milk Chocolate',
-    cost: '$6.49',
-    image: 'https://example.com/chocolate3.jpg',
+    name: 'Celebration',
+    cost: '$21.49',
+    image: '/image/celebration.jpg',
   },
-  // Add more products as needed
+  {
+    id: 4,
+    name: 'Bounty',
+    cost: '$10.49',
+    image: '/image/bounty.jpg',
+  },
+  {
+    id: 5,
+    name: 'Mars',
+    cost: '$10.49',
+    image: '/image/mars.jpg',
+  },
+  {
+    id: 6,
+    name: 'Snickers',
+    cost: '$10.49',
+    image: '/image/snickers.jpg',
+  },
+  {
+    id: 7,
+    name: 'Rafaello',
+    cost: '$18.49',
+    image: '/image/rafaello.jpg',
+  },
+  {
+    id: 8,
+    name: 'Mone',
+    cost: '$6.49',
+    image: '/image/mone.jpg',
+  },
+  {
+    id: 9,
+    name: 'Alyonka',
+    cost: '$14.49',
+    image: '/image/alenka.jpg',
+  },
+  {
+    id: 10,
+    name: 'Kinder',
+    cost: '$18.49',
+    image: '/image/kinder.jpg',
+  },
+  {
+    id: 11,
+    name: 'Alyonka',
+    cost: '$14.49',
+    image: '/image/alenka.jpg',
+  },
+  {
+    id: 12,
+    name: 'Alyonka',
+    cost: '$14.49',
+    image: '/image/alenka.jpg',
+  },
+
 ];
 
 const ProductsPart = () => {
   return (
     <div className="flex flex-wrap justify-center gap-8 p-6">
       {products.map((product) => (
-        <div key={product.id} className="max-w-xs rounded-lg shadow-lg overflow-hidden">
+        <div key={product.id}  className="w-[300px] border border-white text-center pb-2 rounded-lg shadow-lg overflow-hidden">
           <img
             src={product.image}
             alt={product.name}
             className="w-full h-64 object-cover"
           />
           <div className="p-4">
-            <h3 className="text-xl font-semibold">{product.name}</h3>
-            <p className="text-gray-600">{product.cost}</p>
+            <h3 className="text-[#E2603B] font-semibold">{product.name}</h3>
+            <p className="text-[#F9E0D9]">{product.cost}</p>
           </div>
+          <button className="w-[160px] h-[50px] text-[18px] border bg-gradient-to-r from-[#E2603B] via-[#89393B] rounded-tr-[60px] text-[#F9E0D9] rounded-bl-[60px] hover:bg-[#E2603B] duration-300 cursor-pointer max-sm:mx-auto">Add Product</button>
         </div>
       ))}
     </div>
